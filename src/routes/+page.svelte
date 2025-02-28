2cab55ff22baba863a10b6472d82aee42ab9c613
Dean Masley <grins@Deans-MacBook-Pro.local>
2025-02-28 13:56:08 +0100

    add tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,18 @@
 	import Scene from './Scene.svelte';
 </script>
 
-<div class="h-screen w-screen">
+<div class="relative h-screen w-screen">
+	<div class="absolute top-4 z-50 flex w-full items-center justify-between px-6">
+		<div class="scale-100 rounded-full bg-orange-200 px-2 py-1 text-xs text-orange-500 opacity-70">
+			Threlte (svelte & threejs)
+		</div>
+
+		<div
+			class="scale-100 cursor-pointer rounded-full bg-blue-200 px-4 py-3 text-blue-500 opacity-50 transition-all hover:scale-125 hover:opacity-100"
+		>
+			<a href="https://github.com/JollyGrin/threejs-gallery"> git </a>
+		</div>
+	</div>
 	<Canvas>
 		<Scene />
 	</Canvas>
